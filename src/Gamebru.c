038b3ade7d4393418c1791044c5fa53b613f5a37
CPU Governor <cpu.governor@gmail.com>
2024-02-07 18:49:21 +0100

    created gamebru.c

---
+++ src/Gamebru.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "Gamebru.h"
+
+int main ( ){
+	
+	
+	
+	return 0;
+}
---
+++ src/include/Gamebru.h
@@ -0,0 +1,10 @@
+#ifndef GAMEBRU_H
+#define GAMEBRU_H
+
+// Include necessary SDL headers
+#include<SDL2/SDL.h>
+// Function declaration for initializing SDL
+int Bru_Init();
+
+/* GAMEBRU_H */
+#endif 
